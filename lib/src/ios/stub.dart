a56f1a53480892dcfe9cc657cb5af32442496b4f
Koji Wakamiya <koji.wakamiya@gmail.com>
2025-12-23 08:58:30 +0900

    doc: Update dartdoc

--- lib/src/ios/stub.dart
+++ lib/src/ios/stub.dart
@@ -1,7 +1,9 @@
+/// (Stub) Requests permission to display badges on iOS.
 void requestBadgePermission({
   required bool badge,
   required bool sound,
   required bool alert,
 }) {}
 
+/// (Stub) Updates the application icon badge number on iOS.
 void updateBadge(int count) {}
--- lib/src/macos/stub.dart
+++ lib/src/macos/stub.dart
@@ -1,1 +1,2 @@
+/// (Stub) Updates the application icon badge number on macOS.
 void updateBadge(int count) {}
